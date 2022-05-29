376056cb07e75c0416196700a9276eafb0ff0462
Nicolai Skye <nicolaiskye@icloud.com>
2022-05-29 15:29:27 -0400

    Initial Commit

---
+++ nicolaiSnake/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  nicolaiSnake
+//
+//  Created by Nicolai Skye on 5/29/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
