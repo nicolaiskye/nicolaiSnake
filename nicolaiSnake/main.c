e3070291289d25222e5b95091ea25a2d99b9cf84
Nicolai Skye <nicolaiskye@icloud.com>
2022-05-30 21:51:58 -0400

    Add basic player movement, border drawing

--- nicolaiSnake/main.c
+++ nicolaiSnake/main.c
@@ -10,121 +10,157 @@
 
 #define TIMEOUT 300
 
-enum xDirection {
-    Left,
-    Right,
-};
+typedef struct GameContext {
+    int window_height;
+    int window_width;
+    int game_area_height;
+    int game_area_width;
+} gameContext;
 
-enum yDirection {
+typedef struct Position {
+    int x;
+    int y;
+} position;
+
+typedef enum Direction {
     Up,
     Down,
+    Left,
+    Right,
+    None,
+} direction;
+
+struct SnakePiece {
+    position pos;
+    struct SnakePiece *next_piece;
 };
 
-void drawBox(int height, int width, int startx, int starty);
-void drawBorder(int winh, int winw, char *ch);
+void drawBorder(gameContext *context);
+void drawPlayer(void);
 
 int main() {
+    
     int ch;
-    int window_height, window_width;
-    enum xDirection xdir;
-    enum yDirection ydir;
-    
-    
+    struct GameContext context;
+    direction dir;
     
     initscr(); /* start curses mode */
     raw(); /* line buffering disabled */
     noecho(); /* disable echoing of char type by user */
-    getmaxyx(stdscr, window_height, window_width);
+    getmaxyx(stdscr, context.window_height, context.window_width);
     
-    mvprintw(0, 0, "Terminal Width: %d, Terminal Height: %d", window_width, window_height);
+    // Game area is just the window area for now.
+    // Will update later when I add an info panel.
+    const int game_area_height = context.window_height;
+    const int game_area_width = context.window_width;
     
-    mvprintw(2, 0, "Press any key to start");
+    // Game start screen and debug info
+    mvprintw(0, 0, "Terminal Width: %d, Terminal Height: %d", context.window_width, context.window_height);
+    mvprintw(2, 0, "Game Area Width: %d, Game Area Height: %d", game_area_width, game_area_height);
+    mvprintw(4, 0, "Press any key to start");
     move(4, 0);
     ch = getch();
     clear();
     
+    // Timeout acts as timer to run the games FPS
     timeout(TIMEOUT);
-    int x = 10;
-    int y = 10;
-    xdir = Right;
-    ydir = Down;
     
+    // Player starting position and direction
+    const char player_snake_head = '#';
+    position player_pos;
+    position next_block;
+    player_pos.x = (int)game_area_width/2;
+    player_pos.y = (int)game_area_height/2;
+    char player_input;
+    dir = None; // Player starts standing still
+    
+    // Create the 'quit' message and determine its placement on screen
     const char* quitmsg = "Press 'q' to quit.";
-    const int hlquitmsg = (int)strlen(quitmsg)/2;
-    const int halfscreenx = (int)window_width/2;
-    const int xquitmsg = halfscreenx-hlquitmsg;
+    const int quitmsg_halfpoint = (int)strlen(quitmsg)/2;
+    const int window_halfpoint = (int)context.game_area_width/2;
+    const int quitmsg_x_pos = window_halfpoint-quitmsg_halfpoint;
     
-    char* locmsg;
-    int llocmsg;
-    
+    // Game loop
     while(true) {
         clear();
-        locmsg = "x: %d y: %d";
-        llocmsg = (int)strlen(locmsg)/2;
-        int xlocmsg = halfscreenx-llocmsg;
-        mvprintw(0, xlocmsg, locmsg, x, y);
-        mvprintw(2, xquitmsg, quitmsg);
+        mvprintw(2, quitmsg_x_pos, quitmsg);
+                
+        // Move the player head in the desired direction
+        switch(dir) {
+            case Up:
+                player_pos.y -= 1;
+                break;
+            case Down:
+                player_pos.y += 1;
+                break;
+            case Left:
+                player_pos.x -= 1;
+                break;
+            case Right:
+                player_pos.x += 1;
+                break;
+            case None: // Do nothing
+                break;
+        }
+        
+        // Wrap player if past game bounds
+        if (player_pos.x <= 0)
+            player_pos.x = game_area_width-2;
+        if (player_pos.x >= game_area_width-1)
+            player_pos.x = 1;
+        
+        if (player_pos.y <= 0)
+            player_pos.y = game_area_height-2;
+        if (player_pos.y >= game_area_height-1)
+            player_pos.y = 1;
+        
         attron(A_BOLD);
-        mvaddch(y, x, 'D');
-        mvaddch(y, x+1, 'V');
-        mvaddch(y, x+2, 'D');
+        mvaddch(player_pos.y, player_pos.x, player_snake_head);
         attroff(A_BOLD);
-        
-        drawBox(5, 10, 5, 5);
-        drawBox(8, 15, 45, 15);
-        
-        if (x+4 >= window_width)
-            xdir = Left;
-        if (x-1 <= 0)
-            xdir = Right;
-        if (y+2 >= window_height)
-            ydir = Up;
-        if (y-1 <= 0)
-            ydir = Down;
-        
-        if (xdir == Right)
-            x += 1;
-        if (xdir == Left)
-            x -= 1;
-        if (ydir == Down)
-            y += 1;
-        if (ydir == Up)
-            y -= 1;
 
-        drawBorder(window_height, window_width, '#');
+    
+        drawBorder(&context);
         refresh();
-//        move(window_height, window_width); // doesn't seem to work
-        if (getch() == 'q')
+        
+        player_input = getch();
+        if (player_input == 'w')
+            dir = Up;
+        if (player_input == 'a')
+            dir = Left;
+        if (player_input == 's')
+            dir = Down;
+        if (player_input == 'd')
+            dir = Right;
+        
+        // Quit the game
+        if (player_input == 'q')
             break;
     }
     
     endwin(); /* end curses session */
-    
     return 0;
 }
 
-
-void drawBox(int height, int width, int startx, int starty /*, WINDOW *win*/) {
+void drawBorder(gameContext *context) {
     
-//    if (win == NULL)
-//        win = stdscr;
+    // DEBUG
+    mvprintw(2, 2, "ga height: %d", context->game_area_height);
+    mvprintw(4, 2, "ga width: %d", context->game_area_width);
+    // END DEBUG
     
-    for (size_t i = 0; i < height; ++i) {
-        for (size_t j = 0; j < width; ++j) {
-            mvaddch(starty+i, startx+j, '#');
+    for (int wrow = 0; wrow < context->game_area_height; ++wrow) {
+        for (int wcol = 0; wcol < context->game_area_width; ++wcol) {
+            // Left and right sides of border
+            if (wcol == 0 || wcol == context->game_area_width-1)
+                mvaddch(wrow, wcol, '|');
             
+            // Top and bottom of border
+            if (wrow == 0 || wrow == context->game_area_height-1)
+                mvaddch(wrow, wcol, '-');
         }
     }
-    refresh();
 }
 
-
-void drawBorder(int winh, int winw, char *ch) {
-    for (int wrow = 0; wrow < winh; ++wrow) {
-        for (int wcol = 0; wcol < winw; ++wcol) {
-            if (wcol == 0 || wcol == winw-1 || wrow == 0 || wrow == winh-1)
-                mvaddch(wrow, wcol, '#');
-        }
-    }
-    refresh();
+void drawPlayer(void) {
+    
 }
